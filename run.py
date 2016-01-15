6d9d0cd546b4c0497270a32d8c2b141a42b21df4
Sam Lin <samlin@Sams-MBP.hsd1.ca.comcast.net>
2016-01-15 15:32:34 -0800

    second commit

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import twilio.twiml
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-	return "Hello World!"
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+	
+	resp = twilio.twiml.Response()
+	resp.say("hello monkey")
+
+	return str(resp)
 
 if __name__ == "__main__":
 	app.run(debug=True)
